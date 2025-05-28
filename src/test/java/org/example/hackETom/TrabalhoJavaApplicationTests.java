bd24e5b52eb6a496b325ba9b96b1181adc318f7e
Guilherme Fernandes de Souza <guii0410@gmail.com>
2025-05-27 21:50:16 -0300

    inicializacao projeto

---
+++ src/test/java/org/example/hackETom/TrabalhoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hackETom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

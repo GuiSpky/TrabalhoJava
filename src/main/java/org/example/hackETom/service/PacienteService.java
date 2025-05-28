d44bc57854059284ae0b5afb0005eeb7fb38c0dc
Guilherme Fernandes de Souza <guii0410@gmail.com>
2025-05-28 14:06:06 -0300

    criado métodos crude do paciente

--- src/main/java/org/example/hackETom/service/PacienteService.java
+++ src/main/java/org/example/hackETom/service/PacienteService.java
@@ -1,4 +1,32 @@
 package org.example.hackETom.service;
 
+import org.example.hackETom.model.Paciente;
+import org.example.hackETom.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PacienteService {
+
+
+    @Autowired
+    private PacienteRepository repository;
+
+    public void salvar(Paciente paciente) {
+        repository.save(paciente);
+    }
+
+    public List<Paciente> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Paciente buscarPorId(Long id) {
+        return repository.findById(id).get();
+    }
+
+    public void deletarPorId(Long id) {
+        repository.deleteById(id);
+    }
 }

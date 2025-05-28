cf49e7ac786bf892834e82d7d172269f6e60a659
mariaguirelli <eduardaguirelli89@gmail.com>
2025-05-27 23:58:11 -0300

    model e repository de Agenda

---
+++ src/main/java/org/example/hackETom/model/Agenda.java
@@ -0,0 +1,30 @@
+package org.example.hackETom.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Agenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "medico_id", nullable = false)
+    private Medico medico;
+
+    @ManyToOne
+    @JoinColumn(name = "paciente_id", nullable = false)
+    private Paciente paciente;
+
+    private LocalDateTime dataHora;
+}
---
+++ src/main/java/org/example/hackETom/repository/AgendaRepository.java
@@ -0,0 +1,8 @@
+package org.example.hackETom.repository;
+
+import org.example.hackETom.model.Agenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgendaRepository extends JpaRepository<Agenda, Long> {
+
+}
